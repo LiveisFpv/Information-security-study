851a016f0de65fb60f0719df55fc9d9f1cc5b4be
LiveisFpv <nasonovnicolai@gmail.com>
2026-02-05 23:39:22 +0300

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,12 +9,6 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-    base: '/Information-security-study/',
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
     }
   }),
 )
